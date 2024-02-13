aa740e3984f28ea133b932ea90c005aa3f272a4a
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2024-02-13 21:39:47 +0530

    Styles active link in nav bar with classnames@2.3.2

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai';
+import classnames from 'classnames';
 
 const NavBar = () => {
     const currentPath = usePathname();
@@ -21,7 +22,11 @@ const NavBar = () => {
                 { links.map( link => 
                     <Link 
                         key={link.href}
-                        className={`${link.href === currentPath ? 'text-zinc-900': 'text-zinc-500'} hover:text-zinc-800 transition-colors`}
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href !== currentPath,
+                            'hove:text-zinc-800 transition-colors': true
+                        })}
                         href={link.href}
                     >
                         { link.label }
