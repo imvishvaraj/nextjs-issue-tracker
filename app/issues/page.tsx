58fd6c25b23ee5ff8997c8d54952e9c036ccccd1
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2024-02-16 16:57:23 +0530

    removed custom changes

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,24 +4,13 @@ import { Button } from '@radix-ui/themes';
 import Link from 'next/link';
 import IssuesTable from '../components/IssuesTable';
 
-const IssuesPage = () => {
-  const [issues, setIssues] = useState([]);
-  // console.log('issues', issues)
-
-    useEffect(() => {
-      fetch('/api/issues')
-        .then(response => response.json())
-        .then(data => setIssues(data))
-        .catch(error => console.error(error));
-    }, []);
-  
+const IssuesPage = () => {  
 
   return (
     <div className='max-w-xl '>
       <Button>
         <Link href="/issues/new">New Issue</Link>
       </Button>
-      <IssuesTable issues={issues} />
 
     </div>
   )
